8a320bdf04549a054f23357d374df159a744c978
Pratik6666 <ppchowdhury6@gmail.com>
2020-11-24 15:22:04 +0530

    scatter

--- scatterplot.R
+++ scatterplot.R
@@ -1,2 +1,12 @@
 mtcars
+plot(mtcars$hp,mtcars$mpg,main="Regression for HP vs mileage",
+     xlab = "Horsepower",ylab = "Mileage")
+abline(lm(mpg ~ hp, data = mtcars),col='red')
 
+install.packages("car")
+library(car)
+scatterplot(mpg ~ hp | cyl, data = mtcars, xlab = "horsepower", 
+            ylab = "MPG",main="Regression HP vs MPG")
+remove.packages("car")
+library(car)
+remove.packages("car","car")
